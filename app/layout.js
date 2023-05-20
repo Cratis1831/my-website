f924feab070931acbb3edbf0f00cf2cec701c907
Cratis1831 <asotoudeh18@hotmail.com>
2023-05-20 15:57:48 -0400

    prelim design

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import "./globals.css";
 import { Inter } from "next/font/google";
 
@@ -11,7 +12,17 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <nav className="sticky top-0 z-30 w-full backdrop-blur duration-1000 bg-zinc-100/0">
+          <div className="flex items-center justify-end h-16 md:h-20 pr-40 gap-12">
+            <Link href="/">Home</Link>
+            <Link href="/">Portfolio</Link>
+            <Link href="/">About</Link>
+            <Link href="/">Contact</Link>
+          </div>
+        </nav>
+        {children}
+      </body>
     </html>
   );
 }
--- app/page.js
+++ app/page.js
@@ -1,55 +1,80 @@
+import Image from "next/image";
 import Link from "next/link";
+import {
+  BsGithub,
+  BsTwitter,
+  BsInstagram,
+  BsFacebook,
+  BsCloudSun,
+  BsPencil,
+} from "react-icons/bs";
+
+import { MdFitnessCenter } from "react-icons/md";
+
 import React from "react";
 export default function Home() {
   return (
-    <main>
-      <div className="min-h-screen bg-gradient-to-br from-gray-950 to-zinc-500 flex flex-col items-center justify-center">
-        <h1 className="text-4xl font-bold text-white mb-8">
-          Under Construction
+    <main className="bg-white text-zinc-900">
+      <div className="items-center justify-center mt-8">
+        <h1 className="text-center font-extrabold tracking-[-0.02em] py-4 text-6xl lg:text-8xl text-transparent bg-clip-text bg-gradient-to-tr from-zinc-900/60 to-zinc-900">
+          Ashkan Sotoudeh
         </h1>
-        <div className="flex gap-8">
-          <div className="bg-white bg-opacity-30 p-8 rounded-lg shadow-lg">
-            <h2 className="text-2xl font-bold text-white text-center">14</h2>
-            <p className="text-md text-white text-center">days</p>
+
+        <div className="flex items-center justify-center pt-2">
+          <p className="mt-2 text-2xl text-zinc-700 font-light">
+            Mobile Developer
+          </p>
+        </div>
+
+        <div className="flex items-center justify-center gap-4 pt-20">
+          <div className=" transition hover:scale-105 ease-in-out duration-200 cursor-pointer break-inside-avoid rounded-lg border border-gray-300 bg-white/20 bg-clip-padding p-4 backdrop-blur-lg backdrop-filter md:w-[300px] w-full h-fit">
+            <div className="flex items-start justify-start gap-4">
+              <BsCloudSun
+                className="text-4xl text-zinc-700 hover:text-zinc-900"
+                size={24}
+              />
+              <p className="font-bold">Weather App</p>
+            </div>
           </div>
-          <div className="bg-white bg-opacity-30 p-8 rounded-lg shadow-lg">
-            <h2 className="text-2xl font-bold text-white text-center">00</h2>
-            <p className="text-md text-white text-center">hours</p>
+          <div className=" transition hover:scale-105 ease-in-out duration-200 cursor-pointer break-inside-avoid rounded-lg border border-gray-300 bg-white/20 bg-clip-padding p-4 backdrop-blur-lg backdrop-filter md:w-[300px] w-full h-fit">
+            <div className="flex items-start justify-start gap-4">
+              <MdFitnessCenter
+                className="text-4xl text-zinc-700 hover:text-zinc-900"
+                size={24}
+              />
+              <p className="font-bold">Fitness App</p>
+            </div>
           </div>
-          <div className="bg-white bg-opacity-30 p-8 rounded-lg shadow-lg">
-            <h2 className="text-2xl font-bold text-white text-center">00</h2>
-            <p className="text-md text-white text-center">mins</p>
+          <div className=" transition hover:scale-105 ease-in-out duration-200 cursor-pointer break-inside-avoid rounded-lg border border-gray-300 bg-white/20 bg-clip-padding p-4 backdrop-blur-lg backdrop-filter md:w-[300px] w-full h-fit">
+            <div className="flex items-start justify-start gap-4">
+              <BsPencil
+                className="text-4xl text-zinc-700 hover:text-zinc-900"
+                size={24}
+              />
+              <p className="font-bold">Todo App</p>
+            </div>
           </div>
         </div>
-        <h1 className="text-xl font-bold text-white my-8">
-          Check these out in the mean time
-        </h1>
-        <div className="text-sm font-normal text-white flex gap-4">
-          <Link
-            href="https://weather.ash-kan.com"
-            target="_blank"
-            className="hover:scale-110 transition ease-in-out"
-          >
-            Weather App
-          </Link>
-          {" | "}
-
-          <Link
-            href="https://prompt.ash-kan.com"
-            target="_blank"
-            className="hover:scale-110 transition ease-in-out"
-          >
-            AI Prompt Store App
-          </Link>
-          {" | "}
-
-          <Link
-            href="https://todo.ash-kan.com"
-            target="_blank"
-            className="hover:scale-110 transition ease-in-out"
-          >
-            Todo App
-          </Link>
+        <div className="flex items-center justify-center pt-8 gap-8">
+          <BsGithub
+            className="inline-block text-4xl text-zinc-700 hover:text-zinc-900 transition hover:scale-105 ease-in-out duration-200 cursor-pointer"
+            size={24}
+          />
+          <BsTwitter
+            className="inline-block text-4xl text-zinc-700 hover:text-zinc-900 transition hover:scale-105 ease-in-out duration-200 cursor-pointer"
+            size={24}
+          />
+          <BsInstagram
+            className="inline-block text-4xl text-zinc-700 hover:text-zinc-900 transition hover:scale-105 ease-in-out duration-200 cursor-pointer"
+            size={24}
+          />
+          <BsFacebook
+            className="inline-block text-4xl text-zinc-700 hover:text-zinc-900 transition hover:scale-105 ease-in-out duration-200 cursor-pointer"
+            size={24}
+          />
+        </div>
+        <div className="flex items-center justify-center pt-8 gap-8">
+          <p>Site Under Construction...</p>
         </div>
       </div>
     </main>
