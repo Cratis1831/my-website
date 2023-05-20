b279567eb92bfab41d9e607e24b77e653311ec52
Cratis1831 <asotoudeh18@hotmail.com>
2023-05-19 22:06:51 -0400

    bg color

--- app/page.js
+++ app/page.js
@@ -3,13 +3,13 @@ import React from "react";
 export default function Home() {
   return (
     <main>
-      <div className="min-h-screen bg-gradient-to-br from-red-500 to-blue-500 flex flex-col items-center justify-center">
+      <div className="min-h-screen bg-gradient-to-br from-gray-950 to-zinc-500 flex flex-col items-center justify-center">
         <h1 className="text-4xl font-bold text-white mb-8">
           Under Construction
         </h1>
         <div className="flex gap-8">
           <div className="bg-white bg-opacity-30 p-8 rounded-lg shadow-lg">
-            <h2 className="text-2xl font-bold text-white text-center">00</h2>
+            <h2 className="text-2xl font-bold text-white text-center">14</h2>
             <p className="text-md text-white text-center">days</p>
           </div>
           <div className="bg-white bg-opacity-30 p-8 rounded-lg shadow-lg">
@@ -24,7 +24,7 @@ export default function Home() {
         <h1 className="text-xl font-bold text-white my-8">
           Check these out in the mean time
         </h1>
-        <div className="text-md font-normal text-white flex gap-4">
+        <div className="text-sm font-normal text-white flex gap-4">
           <Link
             href="https://weather.ash-kan.com"
             target="_blank"
